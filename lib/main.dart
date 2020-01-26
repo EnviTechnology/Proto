7ada1acf021033b5ef59af097951696300811783
jotiger <haoliang.jiang@outlook.com>
2020-01-26 11:00:34 +1100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_shine/flutter_shine.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double x;
+  double y;
+
+  @override
+  void initState() {
+    super.initState();
+
+    x = -100;
+    y = -100;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GestureDetector(
+          onLongPressMoveUpdate: _onLongPressMoveUpdate,
+          child: Center(
+            child: FlutterShine(
+              config: Config(shadowColor: Colors.cyan),
+              light: Light(intensity: 1, position: Point(x, y)),
+              builder: (BuildContext context, ShineShadow shineShadow) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Text(
+                      "Envi",
+                      style: TextStyle(
+                          fontSize: 130,
+                          color: Colors.grey[350],
+                          shadows: shineShadow?.shadows),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onLongPressMoveUpdate(LongPressMoveUpdateDetails details) {
+    setState(() {
+      this.x = details.offsetFromOrigin.dx;
+      this.y = details.offsetFromOrigin.dy;
+    });
+  }
+}
